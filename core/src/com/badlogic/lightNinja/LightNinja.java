94410443f3b61a1785d92c709b6e24a3e541fea5
Hugo Garrido y Saez <garrido.saez.hugo@gmail.com>
2015-04-23 10:33:18 +0200

    Modification on configuration

--- core/src/com/badlogic/lightNinja/LightNinja.java
+++ core/src/com/badlogic/lightNinja/LightNinja.java
@@ -3,25 +3,42 @@ package com.badlogic.lightNinja;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LightNinja extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Ninja ninja;
 	
 	@Override
 	public void create () {
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		ninja = new Ninja();
+		ninja.create(batch, camera);
+		
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2F, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+			ninja.draw();
 		batch.end();
+		
+		ninja.render();
+	}
+	
+	public void dispose(){
+		ninja.dispose();
 	}
 }
